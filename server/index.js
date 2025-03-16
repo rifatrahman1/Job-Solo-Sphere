99ef8b3feb025ce7572ec101f55a1e77b4f2340e
Rifat Rahman <ahsan30rifat@gmail.com>
2025-03-16 15:54:20 +0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.noaml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+    const db = client.db('solo_db')
+    const jobs_collection = db.collection('jobs')
+
+    // save a job data in db
+    app.post('/add_job', async (req, res) => {
+      const jobData = req.body;
+      const result = await jobs_collection.insertOne(jobData)
+      res.send(result);
+    })
+
+    app.get('/jobs', async (req, res) => {
+      const result = await jobs_collection.find().toArray();
+      res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+app.get('/', (req, res) => {
+  res.send('Hello from SoloSphere Server....')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
