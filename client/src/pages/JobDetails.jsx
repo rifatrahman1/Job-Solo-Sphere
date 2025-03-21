bec2c790b0712201955723f7525aa83c45ffe574
Rifat Rahman <ahsan30rifat@gmail.com>
2025-03-21 19:15:29 +0600

    added to solo sphere job details

--- client/src/pages/JobDetails.jsx
+++ client/src/pages/JobDetails.jsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { format } from "date-fns";
 import { useParams } from 'react-router-dom'
 import DatePicker from 'react-datepicker'
 import axios from 'axios'
 import 'react-datepicker/dist/react-datepicker.css'
+import { AuthContext } from '../providers/AuthProvider';
 
 const JobDetails = () => {
+  const { user } = useContext(AuthContext)
   const [startDate, setStartDate] = useState(new Date())
   const { id } = useParams();
   const [job, set_job] = useState({});
@@ -30,15 +32,23 @@ const JobDetails = () => {
     _id,
     deadline, } = job || {};
 
+  const handle_submit = (event) => {
+    event.preventDefault();
+    const form = form.target;
+    const price = form.price.value;
+    const email = user?.email;
+    const comment = form.comment.value;
+  }
+
   return (
-    <div className='flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto '>
+    <div className='flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto'>
       {/* Job Details */}
       <div className='flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]'>
         <div className='flex items-center justify-between'>
           <span className='text-sm font-light text-gray-800 '>
             Deadline: {deadline && (
               <span className='text-xs font-light text-gray-800 '>
-                 {format(new Date(deadline), "P")}
+                {format(new Date(deadline), "P")}
               </span>
             )}
           </span>
@@ -108,6 +118,7 @@ const JobDetails = () => {
                 id='emailAddress'
                 type='email'
                 name='email'
+                defaultValue={user?.email}
                 disabled
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
